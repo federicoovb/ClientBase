fe10dce39518e5654f2fe3c93db132707ad7fa24
violaboros.federico <violaboros.federico@TAVE-PC31.OSDBSESTO.local>
2021-01-26 13:33:20 +0100

    upload all

---
+++ src/clientbase/ClientBase.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientbase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author violaboros.federico
+ */
+public class ClientBase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Apertura connessione");
+        try {
+            while(true){
+                Socket server = new Socket("127.0.0.1", 6666);
+                InputStream dalServer = server.getInputStream();
+                BufferedReader lettore = new BufferedReader(new InputStreamReader(dalServer));
+
+                String risposta = lettore.readLine();
+                System.out.println("Risposta del server: " + risposta);
+
+                lettore.close();
+                server.close();
+                System.out.println("Chiusura connessione");
+                Thread.sleep(5000);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException e) {
+            
+        }
+    }
+    
+}
